0f0cbca504438d0c947cfd72236c534c9d69fdf5
SegunCodes <shegstix64@gmail.com>
2023-06-11 18:13:39 +0200

    c# variables and datatype

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace MyFirstProgram;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int x;
+        x = 123;
+        int y = 321;
+        int z = x + y;
+        double height = 300.5;
+        bool alive = true;
+        char symbol = '@';
+        String name = "SegunCodes";
+        Console.Write(z);
+        Console.WriteLine("Your height is "+ height + "cm");
+        Console.WriteLine("Are you alive? "+ alive);
+        Console.WriteLine("your symbol is "+ symbol);
+        Console.WriteLine("My name is "+ name);
+        // Console.ReadKey();
+    }
+}
