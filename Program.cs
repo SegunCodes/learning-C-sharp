4a1980d8f59dddc26461ca9dc31eed16ec183d22
SegunCodes <shegstix64@gmail.com>
2023-06-16 17:53:29 +0200

    basic calculator in c#

--- Program.cs
+++ Program.cs
@@ -4,52 +4,54 @@ namespace MyFirstProgram;
 class Program
 {
     static void Main(string[] args)
-    {
-        Random random = new Random();
-        bool playAgain = true;
-        int min = 1;
-        int max = 100;
-        int guess;
-        int number;
-        int guesses;
-        String response;
+    {   
+        do{
+            double num1 = 0;
+            double num2 = 0;
+            double result = 0;
 
-        while (playAgain)
-        {
-            guess = 0;
-            guesses = 0;
-            response = "";
-            number = random.Next(min, max+1);
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Calculator Program");
+            Console.WriteLine("---------------------");
 
-            while (guess != number)
+            Console.WriteLine("Enter number 1: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an option: ");
+            Console.WriteLine("\t+ : Add");
+            Console.WriteLine("\t- : Subtract");
+            Console.WriteLine("\t* : Multiply");
+            Console.WriteLine("\t/ : Divide");
+            Console.Write("Enter an option ");
+
+            switch (Console.ReadLine())
             {
-                Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
-                guess = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Guess: " + guess);
-
-                if (guess > number)
-                {
-                    Console.WriteLine(guess + " is to high!");
-                }
-                else if(guess < number){
-                    Console.WriteLine(guess + " is to low!");
-                }
-                guesses++;
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                    break;
+                case "*":
+                    result = num1 + num2;
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + result);
+                    break;
+                default: 
+                    Console.WriteLine("That was not a valid option");
+                    break;
             }
-            Console.WriteLine("Number: " + number);
-            Console.WriteLine("You WIN");
-            Console.WriteLine("Guesses: " + guesses);
-            Console.WriteLine("Would you like to play again? (Y/N): ");
-            response = Console.ReadLine();
-            response = response.ToUpper();
-            if (response == "Y")
-            {
-                playAgain = true;
-            }else{
-                playAgain = false;
-            }
-        }
-        Console.WriteLine("Thanks for playing");
+            Console.WriteLine("Would you like to continue? (Y/N)");
+        } while (Console.ReadLine().ToUpper() == "Y");
+        Console.WriteLine("Bye");
         Console.ReadKey();
     }
 }
